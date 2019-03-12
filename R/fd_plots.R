653e5f466d4fd8f855a0e6c703b59eb2618a169b
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2019-03-12 12:47:05 -0400

    Add real-ish data in three separate tables

--- R/fd_plots.R
+++ R/fd_plots.R
@@ -7,10 +7,9 @@
 #' - `Replicate` (character): Replicate code. Each replicate contains
 #'   multiple plots with different disturbance treatments.
 #' - `Plot` (integer): Plot ID number. Plots are nested within replicates.
-#' - `Latitude`, `Longitude` (double): Plot coordinates, in decimal
+#' - `Longitude`, `Latitude` (double): Plot coordinates, in decimal
 #'   degrees.
-#' - `Disturbance_severity` (double): Level of disturbance severity,
-#'   expressed as a percent (0 - 100) reduction in the target variable (TBD).
+#' - `Plot_area_m2` (double): Area of the plot, in square meters.
 #'
 #' @return A `data.frame`. See "Details" for column descriptions.
 #' @export
@@ -20,6 +19,56 @@
 fd_plots <- function() {
   read.csv(
     system.file("extdata", "fd_plots.csv",
+                package = "fortedata", mustWork = TRUE),
+    stringsAsFactors = FALSE
+  )
+}
+
+
+#' Return the subplots table.
+#'
+#' @details The columns are as follows:
+#' - `Replicate` (character): Replicate code. Each replicate contains
+#'   multiple plots with different disturbance treatments.
+#' - `Plot` (integer): Plot ID number. Plots are nested within replicates.
+#' - `Subplot` (character): Subplot code. Subplots are nested within plots:
+#' one in the eastern part, one in the western.
+#' - `Longitude`, `Latitude` (double): Subplot coordinates, in decimal
+#'   degrees.
+#' - `Plot_area_m2` (double): Area of the subplot, in square meters.
+#'
+#' @return A `data.frame`. See "Details" for column descriptions.
+#' @export
+#'
+#' @examples
+#' fd_subplots()
+fd_subplots <- function() {
+  read.csv(
+    system.file("extdata", "fd_subplots.csv",
+                package = "fortedata", mustWork = TRUE),
+    stringsAsFactors = FALSE
+  )
+}
+
+
+#' Return the nested subplots table.
+#'
+#' @details The columns are as follows:
+#' - `Subplot` (character): Subplot code. Subplots are nested within plots:
+#' one in the eastern part, one in the western.
+#' - `Nested_subplot` (integer): Nested subplot code. Nested subplots are nested subplots,
+#' which are in turn are within plots:
+#' one in the eastern part, one in the western.
+#' - `Nested_subplot_area_m2` (double): Area of the nested subplot, in square meters.
+#'
+#' @return A `data.frame`. See "Details" for column descriptions.
+#' @export
+#'
+#' @examples
+#' fd_nested_subplots()
+fd_nested_subplots <- function() {
+  read.csv(
+    system.file("extdata", "fd_nested_subplots.csv",
                 package = "fortedata", mustWork = TRUE),
     stringsAsFactors = FALSE
   )
--- tests/testthat/test.fd_plots.R
+++ tests/testthat/test.fd_plots.R
@@ -1,24 +1,57 @@
 context("Loading internal package data")
 
+# UMBS is around 45, -85. This sets a very comfortable bounding
+# box around that, which we should shrink later.
+umbs_lon <- c(-84, -86)
+umbs_lat <- c(45, 46)
+
 test_that("Plots table", {
   dat <- fd_plots()
   expect_s3_class(dat, "data.frame")
 
   with(dat, {
-    # UMBS is around 45, -85. This sets a very comfortable bounding
-    # box around that, which we should shrink later.
-    expect_true(all(Latitude > 40))
-    expect_true(all(Latitude < 50))
-    expect_true(all(Longitude > -180))
-    expect_true(all(Longitude < 0))
+    expect_true(all(Latitude > min(umbs_lat)))
+    expect_true(all(Latitude < max(umbs_lat)))
+    expect_true(all(Longitude > min(umbs_lon)))
+    expect_true(all(Longitude < max(umbs_lon)))
+    expect_true(all(Plot_area_m2 > 0))
 
-    expect_true(all(Disturbance_severity >= 0))
-    expect_true(all(Disturbance_severity <= 100))
-
-    expect_type(Plot, "integer")
     expect_type(Replicate, "character") # factor?
+    expect_type(Plot, "integer")
   })
 
-  # Plot x Replicate should be unique
-  expect_equal(nrow(unique(dat[, c("Plot", "Replicate")])), nrow(dat))
+  # Replicate x Plot should be unique
+  expect_equal(nrow(unique(dat[, c("Replicate", "Plot")])), nrow(dat))
+})
+
+test_that("Sublots table", {
+  dat <- fd_subplots()
+  expect_s3_class(dat, "data.frame")
+
+  with(dat, {
+    expect_true(all(Latitude > min(umbs_lat)))
+    expect_true(all(Latitude < max(umbs_lat)))
+    expect_true(all(Longitude > min(umbs_lon)))
+    expect_true(all(Longitude < max(umbs_lon)))
+    expect_true(all(Subplot_area_m2 > 0))
+
+    expect_type(Replicate, "character") # factor?
+    expect_type(Plot, "integer")
+    expect_type(Subplot, "character") # factor?
+  })
+
+  # Replicate x Plot x Subplot should be unique
+  expect_equal(nrow(unique(dat[, c("Replicate", "Plot", "Subplot")])), nrow(dat))
+})
+
+test_that("Nested sublots table", {
+  dat <- fd_nested_subplots()
+  expect_s3_class(dat, "data.frame")
+
+  with(dat, {
+    expect_true(all(Nested_subplot_area_m2 > 0))
+
+    expect_type(Nested_subplot, "integer") # factor?
+  })
+
 })
