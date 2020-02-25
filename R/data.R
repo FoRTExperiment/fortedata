587142cbeb1fc6568859d3069d86f7e7171bba53
Jeff Atkins <jwatkins6@vcu.edu>
2020-02-25 12:45:50 -0500

    updating with soil co2 efflux

--- R/data.R
+++ R/data.R
@@ -12,13 +12,39 @@
 #'   \code{index}, and \code{value}
 #'
 #' \describe{
-#'   \item{subplotID}{price, in US dollars}
-#'   \item{species}{weight of the diamond, in carats}
+#'   \item{subplotID}{subplot identifier}
+#'   \item{species}{species of leaf analyzed}
 #'   \item{index}{spectral index}
 #'   \item{value}{value corresponding to the spectral index}
 #' }
 #'  @source \url{http://atkinsjeff.github.io}
 "leaf_spectrometry"
 
+#' Soil CO2 Efflux measuremnts and Associated Micrometeorological Measurements
+#'
+#' Soil CO efflux measurements collected with a Li-Cor 8100A
+#' Soil temperature measurements were made at 7 cm depth
+#' soil moisture as volumetric water content msmts were wmade with a Campbell CS620
+#' time domain reflectometer (TDR) at 0 - 20 cm depth.
+#' dateTime	subplotID	nestedPlot	run	soilCO2Efflux	soilTemp	VWC	notes
+
+#' @docType data
+#'
+#' @usage data(soil_efflux)
+#'
+#' @format A data frame with four variables: \code{dateTime} \code{subplotID}, \code{nestedPlot},
+#'   \code{run},\code{soilCO2Efflux}, \code{soilTemp}, \code{VWC} and \code{notes}
+#'
+#' \describe{
+#'   \item{dateTime}{date and time of sample in YYYY-MM-DD HH:MM:SS}
+#'   \item{subplotID}{subplot identifier}
+#'   \item{nestedPlot}{sampling point of collar inside of subplot}
+#'   \item{soilCO2Efflux}{soil carbon dioxide efflux in umol CO2 m^2 s^-1}
+#'   \item{soilTemp}{soil temperature at 7 cm depth in celsius}
+#'   \item{VWC}{volumetric water content from 0 to 20 cm depth}
+#'   \item{notes}{additional information}
+#' }
+#'  @source \url{http://atkinsjeff.github.io}
+"soil_efflux"
 
 
