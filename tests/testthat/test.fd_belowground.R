2d94d6ce6f303fd0dccc49c94c022818920e581c
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2020-03-20 21:58:24 -0400

    Remove belowground micromet - what's the point?
    Data already in soil co2

--- tests/testthat/test.fd_belowground.R
+++ tests/testthat/test.fd_belowground.R
@@ -14,17 +14,3 @@ test_that("Soil CO2 Efflux", {
   expect_true(all(dat$Plot %in% subplots$Plot))
   expect_true(all(dat$Subplot %in% subplots$Subplot))
 })
-
-test_that("Micrometeorology", {
-  dat <- fd_micromet()
-
-  expect_is(dat$DateTime, "POSIXlt")
-  expect_is(dat$soilTemp, "numeric")
-  expect_is(dat$VWC, "numeric")
-
-  # All the replicate/plot/subplot codes should exist
-  subplots <- fd_subplots()
-  expect_true(all(dat$Replicate %in% subplots$Replicate))
-  expect_true(all(dat$Plot %in% subplots$Plot))
-  expect_true(all(dat$Subplot %in% subplots$Subplot))
-})
