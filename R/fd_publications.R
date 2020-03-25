32fada12ab5d0744bab05479f34dbd341aa3ee44
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2020-03-25 06:42:14 -0400

    Clean up and create documentation

--- R/fd_publications.R
+++ R/fd_publications.R
@@ -1,14 +1,14 @@
 # FoRTE bibliography
 
 
-#' Return a list of all FoRTE related publications
+#' Return a list of all FoRTE related publications.
 #'
 #' @details The columns are as follows:
 #' - `Entry` (character): Abbreviated in-text entry in the form of author last name and year
-#' - `PubYear` (integer): Year of publication.
-#' - `Title` (integer): Title of publication.
-#' - `Journal` (character): Journal article was published in.
-#' - `DOI` (character): Digital object identifier
+#' - `PubYear` (integer): Year of publication
+#' - `Title` (character): Title of publication
+#' - `Journal` (character): Journal article was published in
+#' - `DOI` (character): Digital Object Identifier
 #'
 #' @return A `data.frame` or `tibble`. See "Details" for column descriptions.
 #' @export
@@ -16,8 +16,5 @@
 #' @examples
 #' fd_publications()
 fd_publications <- function() {
-  refs <- read_csv_file("fd_pubs.csv")
-
-  #returns tibble
-  weak_as_tibble(refs)
+  read_csv_file("fd_pubs.csv")
 }
