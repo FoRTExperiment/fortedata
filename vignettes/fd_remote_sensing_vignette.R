37f2d8455559ee42659a099c485161f891e887dc
Jeff <jwatkins6@vcu.edu>
2020-12-06 11:41:23 -0500

    working on pkgdown remote sensing vignette issue

--- vignettes/fd_remote_sensing_vignette.R
+++ vignettes/fd_remote_sensing_vignette.R
@@ -1,14 +1,57 @@
 ## ----setup, include = FALSE---------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
-  require(fortedata)
+  require(fortedata),
+  require(ggplot2)
 )
+
+## ----data-availble, fig.width = 6, fig.asp = .62, messages = FALSE, echo = FALSE----
+no_of_records.df <- fd_observations()
+
+no_of_records <- subset(no_of_records.df, table == 'fd_canopy_structure' | table == 'fd_hemi_camera')
+# Heatmap
+#x11(width = 10, height = 8)
+ggplot2::ggplot(no_of_records, ggplot2::aes(x = as.factor(month), y = as.integer(year), fill= no_of_obs)) +
+  ggplot2::geom_tile(ggplot2::aes(fill = no_of_obs), color = "black") +
+  ggplot2::geom_text(ggplot2::aes(label = no_of_obs), color = "white") +
+  ggplot2::coord_equal()+
+  ggplot2::scale_fill_gradient(low = "#450d54", high = "#450d54", na.value = 'white')+
+  #scale_fill_viridis(na.value = 'white')+
+  ggplot2::scale_y_reverse()+
+  ggplot2::theme_minimal()+
+  ggplot2::theme(legend.position = "none")+
+  ggplot2::ylab("Year")+
+  ggplot2::xlab("Month")+
+  ggplot2::ggtitle(paste("No. of observations currently available \nin each remote sensing function as of:", Sys.Date()))+
+  ggplot2::facet_grid(table ~ .,  space = "free")+
+  ggplot2::theme(strip.text.y = element_text(size = 9), strip.background = element_rect(
+    color="black", fill="white", size= 0.5, linetype="solid"))
+
+
+
+## ----fd_canopy_structure------------------------------------------------------
+fd_canopy_structure_summary()
+
+## ----rug, fig.width = 6, fig.asp = .65, messages = FALSE, echo = FALSE--------
+x <- fd_canopy_structure()
+
+ggplot(x, aes(y = rugosity, x = replicate))+
+  geom_boxplot()+
+  xlab("Replicate")+
+  ylab("Canopy Rugosity [m]")
+
 
 ## ----fd_hemi_camera-----------------------------------------------------------
 fd_hemi_camera()
 
-## ----fd_canopy_structure------------------------------------------------------
-fd_canopy_structure()
+## ----cam, fig.width = 6, fig.asp = .65, messages = FALSE, echo = FALSE--------
+x <- fd_hemi_camera()
+
+ggplot(x, aes(y = lai_cam, x = replicate))+
+  geom_boxplot()+
+  xlab("Replicate")+
+  ylab("Leaf Area Index")
+
 
 ## ----fd_ceptometer------------------------------------------------------------
 fd_ceptometer()
