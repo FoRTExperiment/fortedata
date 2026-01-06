b03a98c4cedb9131e2f08df6147e3810d8b88c05
kdorheim <kalyn.dorheim@pnnl.gov>
2026-01-06 17:48:48 -0500

    remove orphaned package

--- tests/testthat/test.metadata.R
+++ tests/testthat/test.metadata.R
@@ -4,22 +4,6 @@ test_that("Metadata function", {
   dat <- fd_metadata()
   # Make sure empty strings are correctly converted to NA
   expect_true(!any(na.omit(dat[["units"]]) == ""))
-  # Make sure that units are parseable
-  parseable <- vapply(
-    dat[["units"]],
-    function(x)
-      is.na(x) ||
-        # Units with chemicals in them are not recognized. Consider exceptions
-        # explicitly to air on the side of caution
-        x %in% c("umol H2O/m2/s", "mmol H2O/m2/s", "umol CO2/m2/s") ||
-        udunits2::ud.is.parseable(x),
-    logical(1)
-  )
-  expect_equal(
-    length(parseable), sum(parseable),
-    label = "Parseable units",
-    expected.label = "Units"
-  )
 
   # Make sure everything in tables column refers to a `fortedata` function,
   # and that subsetting metadata works correctly.
